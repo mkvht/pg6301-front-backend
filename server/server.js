80f5dc6965d0737661ac2bf6f4df6863e988839c
Maria Tan <mkvht@outlook.com>
2022-05-05 14:27:57 +0200

    added husky and prettier

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 
 const app = express();
 
-const server = app. listen(process.env.PORT || 3000, () => {
-    console.log("Server started on http://localhost:${server.address().port}");
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on http://localhost:${server.address().port}");
 });
-
