58ba17169ab9aabb445a63f7820335b3d9efe6de
Maria Tan <mkvht@outlook.com>
2022-05-05 15:47:00 +0200

    serve react from express

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hi</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Application</h1>
+      <div>
+        <div>
+          <Link to={"/login"}>Login</Link>
+        </div>
+        <div>
+          <Link to={"/register"}>Sign up</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello this is login</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
